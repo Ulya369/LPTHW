cab7fd7f1977d855d6c6fc5e407d0bec846f5bfe
Ulya369 <33639067+Ulya369@users.noreply.github.com>
2017-12-18 21:48:10 +0100

    Add files via upload

---
+++ ex45_module.py
@@ -0,0 +1,8 @@
+def make_fun():
+    print ":::::::::::::::  WINNERS ROOM    :::::::::::::::::::|"
+    print ":::::::::::::::  CONGRATULATIONS !!!   :::::::::::::|"
+    print ":::::::::::::::  YOU WON THE GAME !!!   ::::::::::::|"
+
+def dont_make_fun():
+    print ":::::::::::::::  LOSERS ROOM     ::::::::::::::::::|"
+    print ":::::::::::::::  YOU LOST THE GAME !!! ::::::::::::|"
